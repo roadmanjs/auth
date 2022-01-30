4ad5a18446a69922f3a366fb0d20ea4186c923b1
cmuhoza <ceddymuhoza@gmail.com>
2022-01-29 19:00:29 -0500

    add more fields to user schema

--- src/user/User.model.ts
+++ src/user/User.model.ts
@@ -37,6 +37,15 @@ export class UserType {
     address?: string = '';
 
     @Field(() => String, {nullable: true})
+    city?: string = '';
+
+    @Field(() => String, {nullable: true})
+    state?: string = '';
+
+    @Field(() => String, {nullable: true})
+    zipcode?: string = '';
+
+    @Field(() => String, {nullable: true})
     country?: string = '';
 
     @Field(() => String, {nullable: true})

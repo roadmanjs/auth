fb2cf5628969eeed729a5fac8ca5682d68de8575
cmuhoza <ceddymuhoza@gmail.com>
2022-03-10 23:44:44 -0500

    proper isAuth pend/middleware

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,10 +1,18 @@
 import {ContextType, MiddlewareFn} from 'couchset';
 import {get as _get, isEmpty} from 'lodash';
 
+import {log} from '@roadmanjs/logs';
 import {verify} from 'jsonwebtoken';
 
+/**
+ *
+ * @sets context.payload = { userId, iat, exp }
+ * @param next
+ * @returns
+ */
 export const isAuth: MiddlewareFn<ContextType> = ({context}, next) => {
     const authorization = _get(context, 'req.headers.authorization', '');
+    const accessTokenSecret = _get(process.env, 'ACCESS_TOKEN_SECRET', '');
 
     if (isEmpty(authorization)) {
         throw new Error('Not Authenticated');
@@ -12,21 +20,10 @@ export const isAuth: MiddlewareFn<ContextType> = ({context}, next) => {
 
     try {
         const token = authorization.split(' ')[1];
-        const verified = verify(token, process.env.ACCESS_TOKEN_SECRET!);
-
-        // verbose('user is auth', verified);
-        /**
-       {
-        userId: 'xxxxxxx04453',
-        iat: 1616303858,
-        exp: 1616307458
-      }
-     */
-        context.payload = verified as any;
-
-        // TODO set user object to request
+        const verified = verify(token, accessTokenSecret);
+        context.payload = verified;
     } catch (err) {
-        console.log('not authenticated');
+        log('not authenticated');
         throw new Error('not authenticated');
     }
 

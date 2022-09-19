4214a12c829a25e12dd671e267d2436a05cc5a91
ceddybi <ceddymuhoza@gmail.com>
2022-09-19 18:14:21 -0400

    search using regex

--- src/user/User.resolver.public.ts
+++ src/user/User.resolver.public.ts
@@ -1,32 +1,24 @@
 import {Resolver, Arg, Query} from 'couchset';
 import {log} from '@roadmanjs/logs';
-import {UserModel, userModelPublicSelectors, UserType} from './User.model';
+import {UserType} from './User.model';
+import {searchUserPublic} from './User.methods';
 
 @Resolver()
 export class UserResolverPublic {
-    @Query(() => UserType)
+    @Query(() => [UserType])
     async getUser(
         @Arg('id', () => String, {nullable: false}) id: string
-    ): Promise<UserType | null> {
+    ): Promise<UserType[] | null> {
         try {
             const username = id;
 
             log(`GET USER: id,username=${username}`);
 
-            const users = await UserModel.pagination({
-                select: userModelPublicSelectors,
-                where: {
-                    $or: [
-                        {id: {$eq: username}},
-                        {email: {$eq: username}},
-                        {phone: {$eq: username}},
-                        {phone: {$eq: `+1${username}`}},
-                    ],
-                },
-            });
+            const users = await searchUserPublic(id);
+
             log(`users found are users=${users && users.length}`);
 
-            return users && users[0]; // get first document
+            return users;
         } catch (error) {
             console.error(error);
             return null;

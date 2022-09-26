cfeef9f92819bc95b0480ecbcd4f2619786a0044
ceddybi <ceddymuhoza@gmail.com>
2022-09-26 19:42:14 -0400

    REFRESH_TOKEN_EXPIRE and ACCESS_TOKEN_EXPIRE

--- src/user/auth.ts
+++ src/user/auth.ts
@@ -11,7 +11,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
 
 export const createAccessToken = (user: UserType) => {
     return sign({userId: user.id}, process.env.ACCESS_TOKEN_SECRET!, {
-        expiresIn: '3d',
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRE || '3d',
     });
 };
 
@@ -20,7 +20,7 @@ export const createRefreshToken = (user: UserType) => {
         {userId: user.id, tokenVersion: user.tokenVersion},
         process.env.REFRESH_TOKEN_SECRET!,
         {
-            expiresIn: '7d',
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRE || '7d',
         }
     );
 };
